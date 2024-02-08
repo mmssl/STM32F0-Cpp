d4ea6908b280073bd621a853a26a9d9c90408dd5
mmssl <muharremssl.001@gmail.com>
2024-02-08 09:20:27 +0300

    Arch-Linux STM32F0 C++ Led Blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,64 @@
 #include "stm32f0xx_conf.h"
 #include <stdint.h>
-// main.cpp
-
 #include "stm32f0xx.h"
-void delay(int ms);
 
-int main() {
+class GpioPin {
+public:
+    GpioPin(GPIO_TypeDef* port, uint16_t pin) : mPort(port), mPin(pin) {
+        init();
+    }
 
-    // Enable clock for GPIOC
-    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    bool Read() const {
+        return (mPort->IDR & mPin) != 0;
+    }
 
-    // Set PC9 as output
-    GPIOC->MODER |= GPIO_MODER_MODER9_0;
+    void Set() {
+        mPort->BSRR = mPin;
+    }
 
-    while(1) {
-        // Toggle PC9
-        GPIOC->ODR ^= GPIO_ODR_9;
+    void Clear() {
+        mPort->BRR = mPin;
+    }
 
-        // Delay
-        delay(500);
+    void Toggle() {
+        mPort->ODR ^= mPin;
+    }
+
+private:
+    GPIO_TypeDef* mPort;
+    uint16_t mPin;
+
+    void init() {
+        // Enable the clock for the GPIO port
+        if(mPort == GPIOA) RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+        if(mPort == GPIOB) RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+        if(mPort == GPIOC) RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+
+        // Set the pin as a general purpose output
+        mPort->MODER &= ~(3U << (2 * mPin)); // Clear mode
+        mPort->MODER |= (1U << (2 * mPin));  // Set as output
+
+        // Set output type as push-pull
+        mPort->OTYPER &= ~(1U << mPin);
+
+        // Set speed to low
+        mPort->OSPEEDR &= ~(3U << (2 * mPin));
+    }
+};
+
+void delay(int time) {
+    while(time--) {
+        __NOP();
     }
 }
 
-void delay(int ms) {
-	for(int i = 0; i<ms * (SystemCoreClock/8000); i++)
-	{
-		__NOP();
-	}
+int main() {
+    GpioPin led(GPIOC, GPIO_Pin_9);
+
+    while(1) {
+        led.Toggle();
+        delay(1000000);
+    }
 }
 
 
